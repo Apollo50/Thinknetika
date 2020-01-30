d02ffec61ff54529e056d16823e3f2057f5d6121
Eugene <evgeni.valentino@gmail.com>
2020-01-31 00:55:52 +0300

    Mistakes correcting

--- Lesson3/route.rb
+++ Lesson3/route.rb
@@ -1,22 +1,18 @@
 class Route
-
-  attr_accessor :list_of_route, :last_station, :first_station
+  attr_accessor :stations, :last_station, :first_station
 
   def initialize(first_station, last_station)
     @first_station = first_station
     @last_station = last_station
-    @list_of_route = [@first_station, @last_station]
+    @stations = [@first_station, @last_station]
   end
-
   def add_stantion(name)
-    @list_of_route[-2] = name
+    @stations[-2] = name
   end
-
   def del_stantion(name)
-    @list_of_route.delete_at(name)
+    @stations.delete(name)
   end
-
   def show_route
-    list_of_route.each_with_index { |station, index| puts "#{station} is #{index}" }
+    @stations.each_with_index { |station, index| puts "#{station} is #{index}" }
   end
 end
--- Lesson3/train.rb
+++ Lesson3/train.rb
@@ -1,77 +1,54 @@
 class Train
+  attr_accessor :route
+  attr_reader :number, :type, :speed
 
-  attr_accessor :speed, :number_of_wagons, :location
-  attr_reader :number, :type
   def initialize(number, type, number_of_wagons)
-    @train_number = number
-    @train_type = type
+    @number = number
+    @type = type
     @number_of_wagons = number_of_wagons
     @speed = 0
   end
-
   def speed_up
     @speed += 10
-    puts "Too too. Your speed is #{@speed}."
   end
-
   def brake
     @speed = 0
-    puts 'You have stopped.'
   end
-
   def add_wagon
-    if speed == 0
-      self.number_of_wagons += 1
-      puts "The number of wagons of your train is/are #{self.number_of_wagons}"
-    else
-      puts "You can not add a wagon. Your speed is #{@speed}. Please brake before adding."
-    end
+    self.number_of_wagons += 1 if speed == 0
   end
-
   def del_wagon
-    if @speed == 0 && self.number_of_wagons > 0
-      self.number_of_wagons -= 1
-    else
-      puts "You can't delete a wagon. Your speed is #{@speed}." if @speed != 0
-      puts "You can't delete a wagon. The length of the train is #{self.number_of_wagons}." if self.number_of_wagons == 0
-    end
+    self.number_of_wagons -= 1 if @speed == 0 && self.number_of_wagons > 0
   end
-
   def add_route(route)
-    @list_of_route = route
-    self.location = route.first_station.name
+    self.route = route
     route.first_station.take_train(self)
   end
-
   def move_forward
-    @list_of_route.each_with_index  do |station, index|
-      if self.location == station && route.last_station.name != self.location
+    self.route.stations.each_with_index  do |station, index|
+      if station.trains.include?(self) && station != self.route.last_station
         station.send_train(self)
-        route.list_of_route[index + 1].take_train(self)
+        self.route.stations[index + 1].take_train(self)
         break
       end
     end
   end
-
-
   def move_back
-    @list_of_route.each_with_index  do |station, index |
-      if self.location == station && route.first_station.name != self.location
+    self.route.stations.each_with_index  do |station, index |
+      if station.trains.include?(self) && station != self.route.first_station
         station.send_train(self)
-        route.list_of_route[index - 1].take_train(self)
+        self.route.stations[index - 1].take_train(self)
         break
       end
     end
   end
-
   def show_station
-    @list_of_route.each_with_index  do |station, index |
-      if self.location == station
-        puts "The previous station is #{@list_of_route[index - 1]}."
-        puts "The current station is #{@list_of_route[index]}."
-        puts "The next station is #{@list_of_route[index + 1]}."
+    self.route.stations.each_with_index  do |station, index |
+      if station.trains.include?(self)
+        prev_stat = self.route.stations[index - 1]
+        curr_stat = self.route.stations[index]
+        next_stat = self.route.stations[index + 1]
       end
     end
   end
-
 end
